1aa4309855b1bfc1e5a88dfbe5d7dfa6913619be
Patrick Lee <patricklee1138@live.com>
2021-03-24 16:44:01 -0700

    add basic 'addition' to calculator
    Co-authored-by: Brandon Magofna <Magofna68@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// Business logic, which is safe to have before the document finishes loading:
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// User interface logic, which should only be read after the document finishes loading:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+   event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
